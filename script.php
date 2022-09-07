e436cf31cb2dcaed6fc72f013fc04f22fe52b54c
Adrian Dvorsky <dvorsky@wezeo.com>
2022-09-07 20:54:36 +0200

    upravy podla Kozu a male upratovanie

--- script.php
+++ script.php
@@ -11,12 +11,15 @@
     
 <?php
 
+
+
+//-------------- VYTIAHNUTIE DAT ZO SUBORU --------------
+
 $filename = 'studenti.json';
 
-//VYTIAHNUTIE DAT ZO SUBORU
-function getData($filename) {
-    if(is_file($filename)) {
-        $json_arr = json_decode(file_get_contents($filename) , true);
+function getData($file) {
+    if(is_file($file)) {
+        $json_arr = json_decode(file_get_contents($file) , true);
         return $json_arr;
     }
     $json_arr = [];
@@ -24,39 +27,22 @@ function getData($filename) {
 }
 $json_arr = getData($filename);
 
-//vypisanie mien studentov cez array_map
 
-function studentName($student) {
-    return $student['name'];
-}
-
-function emptyArr($json_arr) {
-    if ($json_arr === null) {
-        return 'Array je prazdny';
-    }
-    print_r(array_map('studentName', $json_arr));
-}
-
-echo '<pre>';
-emptyArr($json_arr);
-echo '</pre>';
+//-------------- PRIPOJENIE A ULOZENIE DAT --------------
 
 date_default_timezone_set('Europe/Bratislava');
 
-//DEKODOVANIE, PRIPOJENIE A ULOZENIE DAT
-function addData($json_arr, $filename, $isLate) {
+function addData($json_arr, $file, $isLate) {
     if (date('H:i:s') < '23:59:59' && date('H:i:s') > '21:00:00') {
         die('Nemôžeš sa zapísať!');
     }
     
-    //incrementovanie poradoveho cisla pri zapise studenta
-    if(!isset($json_arr)) {
-        $num = 1;
-    } else {
-        $num = count($json_arr);
-        $num++;
-    }
+    $json_arr = (array) $json_arr;
 
+    //incrementovanie poradoveho cisla pri zapise studenta
+    $num = count($json_arr);
+    $num++;
+    
     $json_arr[] = [
         'name' => $_REQUEST['name'],
         'date' => date('j. F Y'),
@@ -65,9 +51,11 @@ function addData($json_arr, $filename, $isLate) {
         'order' => $num
     ];
     
-    file_put_contents($filename, json_encode($json_arr, JSON_PRETTY_PRINT));
+    file_put_contents($file, json_encode($json_arr, JSON_PRETTY_PRINT));
     return $json_arr;
 }
+
+// -------------- MESKANIE --------------
 
 function isLate() {
     return date('H:i:s') > '08:00:00';
@@ -75,7 +63,8 @@ function isLate() {
 $isLate = isLate();
 
 
-//VYPISANIE DAT
+//-------------- VYPISANIE DAT --------------
+
 function printArrival($json_arr) {
     $lastArray = end($json_arr);
     echo '<h1>Ahoj ' . $lastArray['name'] . '</h1>';
@@ -88,38 +77,69 @@ function printArrival($json_arr) {
 
 printArrival(addData($json_arr, $filename, $isLate));
 
-// zapisovanie iba prichodov do pola
-function arrivalData() {
-    $arrivalFile = 'prichody.json';
-    if(is_file($arrivalFile)) {
-        $arrivalArr = json_decode(file_get_contents($arrivalFile), true);
-    } else {
-    $arrivalArr = [];
-    }
 
-    if ($arrivalArr === null) {
-        $arrivalArr = [];
-    }
-    array_push($arrivalArr, date('H:i:s j. F Y'));
-    
-    //iterovanie cez prichody v poli
-    foreach( $arrivalArr as $value) {
-        $arrivalArr[] = $value . ' meskanie';
-    }
-        
-    file_put_contents($arrivalFile, json_encode($arrivalArr, JSON_PRETTY_PRINT));
+echo '<a href="/academia/index.html">Späť na zápis</a><br />';
+
+
+// ------------------------ KONIEC ------------------------
+
+
+
+//-------------- VYPISANIE MIEN STUDENTOV POMOCOU ARRAY MAP --------------
+
+function studentName($student) {
+    return $student['name'];
 }
 
-arrivalData();
+function emptyArr($json_arr) {
+    $json_arr = (array) $json_arr;
+    print_r(array_map('studentName', $json_arr));
+}
+
+echo '<pre>';
+emptyArr($json_arr);
+echo '</pre>';
+
+// ------------------------ KONIEC ------------------------
 
 
 
+// -------------- ZAPISOVANIE PRICHODOV DO POLA --------------
+
+$arrivalData = 'prichody.json';
+$arrivalArr = getData($arrivalData);
+
+function addArrivalData($arrivalArr, $arrivalData) {
+    $arrivalArr = (array) $arrivalArr;
+    array_push($arrivalArr, date('H:i:s j. F Y'));
+    
+      file_put_contents($arrivalData, json_encode($arrivalArr, JSON_PRETTY_PRINT));
+    return $arrivalArr;
+}
+
+addArrivalData($arrivalArr, $arrivalData);
+// ------------------------ KONIEC ------------------------
+
+
+//  SKUSANIE a ROZROBENE -------------- ITEROVANIE --------------
+
+$lateArrival = getData($arrivalData);
+
+foreach( $lateArrival as $value) {
+    $lateArrival = $lateArrival . ' meskanie';
+}
+
+
+echo '<pre>';
+print_r($lateArrival);
+echo '</pre>';
+
+// ------------------------ KONIEC ------------------------
 
 ?>
 
 <br />
 <br />
-<a href="/academia/index.html">Späť na zápis</a>
 
 </body>
 </html>
